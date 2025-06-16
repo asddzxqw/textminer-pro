450026fa70a9a1a5632bebf9015934ac00a7e641
asddzxqw <kk020929@naver.com>
2025-06-16 18:13:51 +0900

    .

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="textminer-pro-asddzxqw",
-    version="0.1.0",
+    name="textminer-pro-asddzxqw-test",
+    version="0.1.1",
     author="asddzxqw",
     author_email="kk020929@naver.com",
     description="텍스트 분석을 위한 Python 패키지",
